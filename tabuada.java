c4c84cbe9dbd10e2995c4c2b76d8a82d2bea3d3b
Daniel Prazeres <dmarquesp96@gmail.com>
2021-08-19 16:29:33 -0300

    tabuada.java
    Semana 1: Introdução ao Java, Instalação e conta Github

---
+++ tabuada.java
@@ -0,0 +1,12 @@
+/*Nome: Daniel Marques Prazeres
+Matrícula: 0050017170*/
+
+public class tabuada{
+    public static void main(String[] args) {
+        
+        for(int i=0;i<=10;i++) {
+            System.out.println("10 * "+i + " = " +(10*i));
+        }
+
+    }
+}
