e6090718aff2a28512e33d4e44701f96dd0678c2
NatanLemes <57756462+NatanLemes@users.noreply.github.com>
2021-09-16 14:44:30 -0300

    Trabalho POO - final

---
+++ CodigoRefatorado/CodigoRefatorado/src/TesteEscalacao.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TesteEscalacao {
+    public static void main(String[] args){
+
+    var entrada = new Scanner(System.in);
+
+
+    Time time = new Time();
+    System.out.print("Nome do time: ");
+    time.setTimeNome(entrada.next());
+    
+    ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+    for(int i=0; i<5;i++)
+    {
+        Jogador jogadorRodada = new Jogador();
+
+        System.out.print("Nome do jogador: ");
+        jogadorRodada.setNomeJog(entrada.next());
+        System.out.print("Sobrenome do Jogador: ");
+        jogadorRodada.setSobreNome(entrada.next());
+        System.out.print("Idade: ");
+        jogadorRodada.setIdadeJogador(entrada.nextInt());
+        jogadorRodada.setTime(Time.getNome());
+        System.out.println("----------------------------------- \n");
+
+        jogadores.add(jogadorRodada);
+    }
+    
+
+    Escalacao jogo = new Escalacao();
+    System.out.print("Data que vai acontecer o jogo: ");
+    jogo.definirData(entrada.next());
+    jogo.definirEscalacao(time, jogadores, jogo.getData());
+    
+}
+}
---
+++ CodigoRefatorado/CodigoRefatorado/src/Time.java
@@ -0,0 +1,21 @@
+public class Time {
+
+    //atributos
+    private static String nome;
+    private String escudo;
+
+    public Time(){
+        this.escudo = "Vermelho e Preto";
+    }
+
+    public static String getNome(){
+        return nome;
+    }
+    public String getEscudo(){
+        return escudo;
+    }
+    public void setTimeNome(String time){
+        Time.nome = time;
+    }
+    
+}
