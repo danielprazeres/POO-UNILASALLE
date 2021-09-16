c4d17b4876d0ddc099c00dd550b6462d34a34d77
Daniel Prazeres <dmarquesp96@gmail.com>
2021-09-16 15:34:48 -0300

    request-nome-usuario
    Modificamos o código para solicitar o nome dos jogadores ao usuário
    
    Co-Authored-By: NatanLemes <57756462+NatanLemes@users.noreply.github.com>

--- CodigoRefatorado/src/Escalacao.java
+++ CodigoRefatorado/src/Escalacao.java
@@ -1,12 +1,35 @@
 import java.util.ArrayList;
 
 public class Escalacao {
-public static ArrayList<String> escalacao;
+// public static ArrayList<String> escalacao;
+private String dataJogo;
+private ArrayList<Jogador> escalacaoTime;
+private Time timeEscalado;
     
-public static ArrayList<String> setEscalacao(){
+public static ArrayList<String> listaNomes(){
     return Jogador.getListaJogador();
 }
-public static void imprimirEscalacao(){
-        System.out.println("A escalação do " + Time.getNome() + " é " + setEscalacao());
+
+
+public static void imprimirEscalacao(String data){
+        System.out.println("A escalação do " + Time.getNome() + " para o dia "+ data +"-------------------------------------------------------------  \n " + listaNomes() );
     }
+
+
+ public void definirData(String dataDoJogo){
+     this.dataJogo = dataDoJogo;
+ }
+
+public void definirEscalacao(Time time, ArrayList<Jogador> jogadores, String data)
+{
+    this.timeEscalado = time;
+    this.escalacaoTime = jogadores;
+    imprimirEscalacao(data);
+}
+
+public String getData()
+{
+    return this.dataJogo;
+}
+
 }
